fdbf0279b4eca90792a3aeadac9ab496b6dcea44
unknown <Lavanya.joga@amzur.com>
2022-01-11 06:25:15 +0530

    secondcommit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,11 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_toast_plugin/flutter_toast_plugin.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  _MyAppState createState() => new _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
@@ -43,13 +45,13 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: new Center(
-          child: new Text('Running\n'),
+        body: const Center(
+          child: Text('Running\n'),
         ),
       ),
     );
--- lib/flutter_toast_plugin.dart
+++ lib/flutter_toast_plugin.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterToast {
-  static const MethodChannel _channel = const MethodChannel('flutter_toast');
+  static const MethodChannel _channel =  MethodChannel('flutter_toast');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
